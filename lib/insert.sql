f57401b6b493ec4f71d42b1c687def9c07ed001a
Dominique Skaf <doskaf@yahoo.com>
2019-10-14 21:06:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Rowdy', 8, 'Male', 'Light Brown', 'Rambunctious', 1)
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 7, 'Female', 'White', 'Diva', 1)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Mr. Chocolate', 2, 'Male', 'Cocoa Brown', 'Sweet', 1)
